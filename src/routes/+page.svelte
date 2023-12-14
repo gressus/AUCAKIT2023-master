23f581c7a359cb265a61f45d69f516b07419e499
Igor Dolgov <gressus@gmail.com>
2023-12-14 19:45:49 +0600

    clear code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,36 @@
 
 <section class="s1"> 
-    test 1. No background image displayed in the built project.
+  test 1. No background image displayed in the built project.
 </section>
 <section class="s2"> 
-    test 2
+  test 2
 </section>
 <section style="min-height: 100vh;width: 100%;background-image: url('2.jpg') !important;background-position: center;background-size: cover;position: relative;"> 
-    test 3
+  test 3
 </section>
 
 <ul><li>No image displayed in the built project.</li></ul>
 
 <style>
-.s1 {
+  .s1 {
     min-height: 100vh;
     width: 100%;
     background-image: url('2.jpg') !important;
     background-position: center;
     background-size: cover;
     position: relative;    
-}
+  }
 
-.s2 {
+  .s2 {
     min-height: 100vh;
     width: 100%;
     background-image: url('/2.jpg') !important;
     background-position: center;
     background-size: cover;
     position: relative;
-}
+  }
 
-li {
-  list-style-image: url('favicon.png');
-}
+  li {
+    list-style-image: url('favicon.png');
+  }
 </style>
