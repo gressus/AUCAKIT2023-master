ba7812f0ddc94f7ec6c141b2f8cbb79f99478a10
Igor Dolgov <gressus@gmail.com>
2023-12-14 16:13:56 +0600

    rename to 2.jpg

--- src/routes/1/+page.svelte
+++ src/routes/1/+page.svelte
@@ -40,8 +40,7 @@
 .headerOfWebSite {
     min-height: 100vh;
     width: 100%;
-    background-image: url('Pamir-Mountains-Tajikistan.jpg') !important;
-    /* background-image: linear-gradient(rgba(4,9,30,0.7), rgba(4,9,30,0.7)), url('Pamir-Mountains-Tajikistan.jpg'); */
+    background-image: url('2.jpg') !important;
     background-position: center;
     background-size: cover;
     position: relative;
